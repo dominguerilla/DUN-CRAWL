6e5881466ac8d5c9884432c429a92fb9b1eb9ff5
cdom <dominguerilla@gmail.com>
2018-06-27 14:39:16 -0400

    can generate whole floor now

--- Assets/FloorGenerator.cs
+++ Assets/FloorGenerator.cs
@@ -25,16 +25,27 @@ public class FloorGenerator : MonoBehaviour {
             Destroy(this);
         }
 
-        GenerateFloor();
+        StartCoroutine(GenerateFloor());
 	}
 
-    void GenerateFloor() {
+    IEnumerator GenerateFloor() {
         GameObject firstFloor = GameObject.Instantiate<GameObject>(floor);
-        BoxCollider box = firstFloor.GetComponent<BoxCollider>();
+        BoxCollider firstBox = firstFloor.GetComponent<BoxCollider>();
 
-        Vector3 topLeftCornerOfFloor = anchor.transform.TransformPoint(box.center - new Vector3(-box.size.x / 2, 0, box.size.z / 2));
+        Vector3 topLeftCornerOfFloor = anchor.transform.TransformPoint(firstBox.center - new Vector3(-firstBox.size.x / 2, 0, firstBox.size.z / 2));
         firstFloor.transform.position = topLeftCornerOfFloor; 
-        
+
+        yield return new WaitForSeconds(0.1f);
+
+        for(int z = 0; z < length; z++) {
+            for(int x = 0; x < width; x++) {
+                //skip the first floor piece, since we already made it
+                if(x == 0 && z == 0)
+                    continue;
+                GameObject currentFloor = GameObject.Instantiate<GameObject>(floor);
+                currentFloor.transform.position = firstFloor.transform.position + new Vector3(firstBox.size.x * x, 0, -firstBox.size.z * z);
+            }
+        }
     }
 	
 }
