7d9d6a9e0d8a0c8ccb34caf635061215867f7e24
cdom <dominguerilla@gmail.com>
2018-06-27 15:15:16 -0400

    navmesh bakes, floor pieces rotate

--- Assets/FloorGenerator.cs
+++ Assets/FloorGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FloorGenerator : MonoBehaviour {
     
@@ -16,8 +17,10 @@ public class FloorGenerator : MonoBehaviour {
     /// <summary>
     /// Should have a box collider.
     /// </summary>
-    public GameObject floor;
+    public GameObject floorPrefab;
 
+    GameObject floorParent;
+    List<GameObject> floorPieces;
 	// Use this for initialization
 	void Start () {
 	    if(!anchor) {
@@ -25,27 +28,40 @@ public class FloorGenerator : MonoBehaviour {
             Destroy(this);
         }
 
+        floorPieces = new List<GameObject>();
         StartCoroutine(GenerateFloor());
 	}
 
     IEnumerator GenerateFloor() {
-        GameObject firstFloor = GameObject.Instantiate<GameObject>(floor);
-        BoxCollider firstBox = firstFloor.GetComponent<BoxCollider>();
+        floorParent = new GameObject("Floor Pieces");
+        GameObject firstFloor = GameObject.Instantiate<GameObject>(floorPrefab);
+        floorPieces.Add(firstFloor);
+        firstFloor.transform.parent = floorParent.transform;
 
+        BoxCollider firstBox = firstFloor.GetComponent<BoxCollider>();
         Vector3 topLeftCornerOfFloor = anchor.transform.TransformPoint(firstBox.center - new Vector3(-firstBox.size.x / 2, 0, firstBox.size.z / 2));
         firstFloor.transform.position = topLeftCornerOfFloor; 
 
-        yield return new WaitForSeconds(0.1f);
+        //yield return new WaitForSeconds(0.1f);
 
         for(int z = 0; z < length; z++) {
             for(int x = 0; x < width; x++) {
                 //skip the first floor piece, since we already made it
                 if(x == 0 && z == 0)
                     continue;
-                GameObject currentFloor = GameObject.Instantiate<GameObject>(floor);
+                GameObject currentFloor = GameObject.Instantiate<GameObject>(floorPrefab);
                 currentFloor.transform.position = firstFloor.transform.position + new Vector3(firstBox.size.x * x, 0, -firstBox.size.z * z);
+                int rotation = Random.Range(0, 4);
+                currentFloor.transform.Rotate(currentFloor.transform.up, 90 * rotation);
+                floorPieces.Add(currentFloor);
+                currentFloor.transform.parent = floorParent.transform;
+                //yield return new WaitForSeconds(0.1f);
             }
         }
+
+        firstFloor.GetComponent<NavMeshSurface>().BuildNavMesh();
+
+        yield return null;
     }
 	
 }
